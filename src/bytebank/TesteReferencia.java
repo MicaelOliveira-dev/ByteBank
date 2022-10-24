bb504bab7e9152a39e796b1a4d1e735d84b9c0bc
MicaelOliveira370 <micael.oliveira@veri.com.br>
2022-10-24 14:02:09 -0300

    Modulo dis finalizado

---
+++ src/bytebank/TesteReferencia.java
@@ -0,0 +1,18 @@
+package bytebank;
+
+public class TesteReferencia {
+
+	public static void main(String[] args) {
+		Conta primeiraConta = new Conta();
+		primeiraConta.saldo = 300;
+		
+		System.out.println("Saldo da primeira: " + primeiraConta.saldo);
+		
+		Conta segundaConta = primeiraConta;
+		System.out.println(segundaConta.saldo);
+		segundaConta.saldo += 100;
+		
+		System.out.println(primeiraConta.saldo);
+	}
+	
+}
