09179c47b98fbc980ab9313eb2ab01e583b18d48
MicaelOliveira370 <micael.oliveira@veri.com.br>
2022-10-26 15:02:48 -0300

    modulo 5

---
+++ bytebankEncapsulado/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author veri-pc
+ *
+ */
+module bytebankEncapsulado {
+}
--- bytebankcomposto/src/bytebankcomposto/TestaContaSemCliente.java
+++ bytebankcomposto/src/bytebankcomposto/TestaContaSemCliente.java
@@ -4,7 +4,6 @@ public class TestaContaSemCliente {
 
 	public static void main(String[] args) {
 		Conta contaDaMarcela = new Conta();
-		System.out.println(contaDaMarcela.saldo);
 		
 		contaDaMarcela.titular = new Cliente();
 		System.out.println(contaDaMarcela.titular);
